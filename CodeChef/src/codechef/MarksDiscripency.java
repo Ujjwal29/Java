3873103549ebccb8e48d5827c5402c98f062dd90
Ujjwal Jain <zpokmn18@gmail.com>
2015-09-17 11:22:05 +0530

    Update MarksDiscripency.java

--- CodeChef/src/codechef/MarksDiscripency.java
+++ CodeChef/src/codechef/MarksDiscripency.java
@@ -8,10 +8,8 @@ public class MarksDiscripency {
         int testcases = sc.nextInt();
         
         int list_sub[][] = new int[testcases][5];
-        //int list_mark[] = new int[testcases];
         int a;
         String s = "";
-        //String[] words = new String[2];
         for (int i = 1; i < testcases+1; i++) {
             s = sc.nextLine();
             String[] words = new String[2];
@@ -19,16 +17,10 @@ public class MarksDiscripency {
             
             a = Integer.parseInt(words[0]);
             list_sub[i][a-65] = words[1].charAt(0); 
-            //list_mark[i] = sc.nextInt();
-            
+           
         }
         for (int i = 1; i < list_sub.length; i++) {
               System.out.println(list_sub[i][0]);  
-//            System.out.println(list_sub.length);
-//            System.out.print(list_sub[i]);
-//            //System.out.print("\t"+list_mark[i]);
-//            System.out.println("");
-            
         }
     
     }
